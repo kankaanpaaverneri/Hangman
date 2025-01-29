968706ceff6887f4cdeb51dd1bd0353fb8314674
Verneri Kankaanpää <verneri.kankaanpaa@gmail.com>
2025-01-29 17:25:00 +0200

    Write tests and clean reveal_ascii_art function.

--- src/hangman.rs
+++ src/hangman.rs
@@ -1,3 +1,25 @@
+//if attempt_count == 1 {
+//display_array(ascii_art, 11, 0, ASCII_HEIGHT, ASCII_WIDTH, false);
+//}
+//if attempt_count == 2 {
+//display_array(ascii_art, 1, 0, 11, 22, true);
+//display_array(ascii_art, 11, 0, ASCII_HEIGHT, ASCII_WIDTH, false);
+//}
+//if attempt_count == 3 {
+//display_array(ascii_art, 0, 0, 2, ASCII_WIDTH, false);
+//display_array(ascii_art, 1, 0, 11, 22, true);
+//display_array(ascii_art, 11, 0, ASCII_HEIGHT, ASCII_WIDTH, false);
+//}
+//if attempt_count == 4 {
+//display_array(ascii_art, 0, 0, 4, ASCII_WIDTH, false);
+//display_array(ascii_art, 1, 0, 9, 22, true);
+//display_array(ascii_art, 11, 0, ASCII_HEIGHT, ASCII_WIDTH, false);
+//}
+//if attempt_count == 5 {
+// Display the whole ascii art
+//display_array(ascii_art, 0, 0, ASCII_HEIGHT, ASCII_WIDTH, false);
+//}
+
 pub mod game {
     const ASCII_WIDTH: usize = 38;
     const ASCII_HEIGHT: usize = 17;
@@ -69,26 +91,35 @@ pub mod game {
         return found_equal;
     }
 
-    fn reveal_ascii_art(count: usize, ascii_art: &[[char; ASCII_WIDTH]; ASCII_HEIGHT]) {
-        if count == 5 {
-            display_array(ascii_art, 11, 0, ASCII_HEIGHT, ASCII_WIDTH, false);
-        }
-        if count == 4 {
-            display_array(ascii_art, 1, 0, 11, 22, true);
-            display_array(ascii_art, 11, 0, ASCII_HEIGHT, ASCII_WIDTH, false);
-        }
-        if count == 3 {
-            display_array(ascii_art, 0, 0, 2, ASCII_WIDTH, false);
-            display_array(ascii_art, 1, 0, 11, 22, true);
-            display_array(ascii_art, 11, 0, ASCII_HEIGHT, ASCII_WIDTH, false);
-        }
-        if count == 2 {
-            display_array(ascii_art, 0, 0, 4, ASCII_WIDTH, false);
-            display_array(ascii_art, 1, 0, 9, 22, true);
-            display_array(ascii_art, 11, 0, ASCII_HEIGHT, ASCII_WIDTH, false);
-        }
-        if count == 1 {
-            display_array(ascii_art, 0, 0, ASCII_HEIGHT, ASCII_WIDTH, false);
+    fn reveal_ascii_art(attempt_count: usize, ascii_art: &[[char; ASCII_WIDTH]; ASCII_HEIGHT]) {
+        let stages: [(usize, usize, usize, usize, bool); 11] = [
+            (0, 0, ASCII_HEIGHT, ASCII_WIDTH, false),
+            (11, 0, ASCII_HEIGHT, ASCII_WIDTH, false),
+            (1, 0, 11, 22, true),
+            (11, 0, ASCII_HEIGHT, ASCII_WIDTH, false),
+            (0, 0, 2, ASCII_WIDTH, false),
+            (1, 0, 11, 22, true),
+            (11, 0, ASCII_HEIGHT, ASCII_WIDTH, false),
+            (0, 0, 4, ASCII_WIDTH, false),
+            (1, 0, 9, 22, true),
+            (11, 0, ASCII_HEIGHT, ASCII_WIDTH, false),
+            (0, 0, ASCII_HEIGHT, ASCII_WIDTH, false),
+        ];
+        let call_counts: [usize; 6] = [0, 1, 2, 3, 3, 1];
+        let call_count = call_counts[attempt_count];
+
+        let iterations: [usize; 6] = [0, 1, 2, 4, 7, 10];
+        let iterator = iterations[attempt_count];
+        for i in 0..call_count {
+            let (column_min, row_min, column_max, row_max, enter_new_line) = stages[iterator + i];
+            display_array(
+                ascii_art,
+                column_min,
+                row_min,
+                column_max,
+                row_max,
+                enter_new_line,
+            );
         }
     }
     fn write_ascii_string_to_array(ascii_art: &str) -> [[char; ASCII_WIDTH]; ASCII_HEIGHT] {
@@ -148,25 +179,25 @@ pub mod game {
     }
 
     pub fn start_hangman(correct_word: &str) -> std::io::Result<()> {
-        let mut attempt_count = 6;
+        let mut attempt_count = 0;
         let ascii_art_buffer = read_file_content(ASCII_ART)?;
         let ascii_art_array = write_ascii_string_to_array(&ascii_art_buffer);
         let mut correct_guesses: HashSet<char> = HashSet::new();
         let mut all_guesses: HashSet<char> = HashSet::new();
-        while attempt_count > 1 {
+        while attempt_count < 5 {
             display_hidden_word(correct_word, &correct_guesses);
             let input = read_input();
             clear_screen()?;
             if let Ok(input) = input {
                 if !compare_characters(&mut correct_guesses, input, correct_word) {
-                    attempt_count -= 1;
+                    attempt_count += 1;
                 }
                 if !all_guesses.contains(&input) {
                     all_guesses.insert(input);
                 }
                 display_used_words(&all_guesses);
 
-                if attempt_count < 6 {
+                if attempt_count > 0 {
                     reveal_ascii_art(attempt_count, &ascii_art_array);
                 }
 
@@ -184,5 +215,52 @@ pub mod game {
         }
         println!("Correct word: {}", correct_word);
         Ok(())
+    }
+    #[cfg(test)]
+    mod tests {
+        use std::collections::HashSet;
+
+        fn insert_word_as_characters(word: &str) -> HashSet<char> {
+            let mut guesses = HashSet::new();
+            for character in word.chars() {
+                guesses.insert(character);
+            }
+            return guesses;
+        }
+
+        use super::{check_win_status, compare_characters};
+        #[test]
+        fn test_check_winning_status_should_succeed() {
+            let guesses = insert_word_as_characters("makkaraperunat");
+            let status = check_win_status("makkaraperunat", &guesses);
+            assert_eq!(true, status);
+        }
+
+        #[test]
+        fn check_win_status_should_fail() {
+            let guesses = insert_word_as_characters("koira");
+            let status = check_win_status("saukko", &guesses);
+            assert_eq!(false, status);
+        }
+
+        #[test]
+        fn inserts_correct_unique_word_to_set() {
+            let guess = 'm';
+            let mut correct_guesses = insert_word_as_characters("sieni");
+
+            let result = compare_characters(&mut correct_guesses, guess, "sienimetsä");
+            assert_eq!(result, true);
+            assert!(correct_guesses.contains(&guess));
+        }
+
+        #[test]
+        fn does_not_insert_if_answer_is_wrong() {
+            let guess = 'y';
+            let mut correct_guesses = insert_word_as_characters("sieni");
+
+            let result = compare_characters(&mut correct_guesses, guess, "sienimetsä");
+            assert_eq!(result, false);
+            assert_eq!(correct_guesses.contains(&guess), false);
+        }
     }
 }
